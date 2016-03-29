740303989c0aa5c3b83e20f2906cbca966620bc0
Bhanu.Rattan <Bhanu.Rattan@SLW10847.spanservices.com>
2016-03-29 11:00:51 +0530

    new db

--- src/main/resources/com/evry/dashboard/sql/masterDB.sql
+++ src/main/resources/com/evry/dashboard/sql/masterDB.sql
@@ -2,77 +2,96 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-SHOW WARNINGS;
-SHOW WARNINGS;
-
-create database report;
-use report;
+CREATE SCHEMA IF NOT EXISTS `report` DEFAULT CHARACTER SET latin1 ;
+USE `report` ;
 
 -- -----------------------------------------------------
-
+-- Table `report`.`login_info`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `user_details` ;
+DROP TABLE IF EXISTS `report`.`login_info` ;
 
-SHOW WARNINGS;
-CREATE  TABLE IF NOT EXISTS `userdetails` (
-  `userid` INT(11) NOT NULL AUTO_INCREMENT ,
-  `username` VARCHAR(45) NULL DEFAULT NULL ,
-  `password` VARCHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`userid`) )
+CREATE  TABLE IF NOT EXISTS `report`.`login_info` (
+  `SNO.` INT(11) NOT NULL AUTO_INCREMENT ,
+  `U_ID` INT(11) NULL DEFAULT NULL ,
+  `LOGIN` TIMESTAMP NULL DEFAULT NULL ,
+  `LOGOUT` TIMESTAMP NULL DEFAULT NULL ,
+  PRIMARY KEY (`SNO.`) ,
+  UNIQUE INDEX `s.no_UNIQUE` (`SNO.` ASC) ,
+  INDEX `LOGIN_FK1_idx` (`U_ID` ASC) )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
-SHOW WARNINGS;
-CREATE UNIQUE INDEX `id_UNIQUE` ON `userdetails` (`userid` ASC) ;
-
-SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `logininfo`
+-- Table `report`.`project_details`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `logininfo` ;
+DROP TABLE IF EXISTS `report`.`project_details` ;
 
-SHOW WARNINGS;
-CREATE  TABLE IF NOT EXISTS `logininfo` (
-  `sno` INT(11) NOT NULL AUTO_INCREMENT ,
-  `uid` INT(11) NULL DEFAULT NULL ,
-  `login` TIMESTAMP NULL DEFAULT NULL ,
-  `logout` TIMESTAMP NULL DEFAULT NULL ,
-  PRIMARY KEY (`sno`) )
+CREATE  TABLE IF NOT EXISTS `report`.`project_details` (
+  `OID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `PROJECT_NAME` VARCHAR(45) NULL DEFAULT NULL ,
+  `PROJECT_STATUS` VARCHAR(45) NULL DEFAULT NULL ,
+  PRIMARY KEY (`OID`) ,
+  UNIQUE INDEX `proid_UNIQUE` (`OID` ASC) )
 ENGINE = InnoDB
+AUTO_INCREMENT = 7
 DEFAULT CHARACTER SET = latin1;
 
-SHOW WARNINGS;
-CREATE UNIQUE INDEX `s.no_UNIQUE` ON `logininfo` (`sno` ASC) ;
-
-SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `task_deatils`
+-- Table `report`.`user_details`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `task_deatils` ;
+DROP TABLE IF EXISTS `report`.`user_details` ;
 
-SHOW WARNINGS;
-CREATE  TABLE IF NOT EXISTS `task_deatils` (
-  `O_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `u_id` INT(11) NULL DEFAULT NULL ,
-  `project_name` VARCHAR(45) NULL DEFAULT NULL ,
-  `week_no` INT(11) NULL DEFAULT NULL ,
-  `planned_task` VARCHAR(45) NULL DEFAULT NULL ,
-  `completed_task` VARCHAR(45) NULL DEFAULT NULL ,
-  `hold_task` VARCHAR(45) NULL DEFAULT NULL ,
-  `inprogress_task` VARCHAR(45) NULL DEFAULT NULL ,
-  `risk_desc` VARCHAR(700) NULL DEFAULT NULL ,
-  `risk_resp` VARCHAR(45) NULL DEFAULT NULL ,
-  `shared_resources` VARCHAR(745) NULL DEFAULT NULL ,
-  `build_resources` VARCHAR(45) NULL DEFAULT NULL ,
-  `project_inc` VARCHAR(45) NULL DEFAULT NULL ,
-  `project_dec` VARCHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`O_id`) )
+CREATE  TABLE IF NOT EXISTS `report`.`user_details` (
+  `O_ID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `USER_NAME` VARCHAR(45) NULL DEFAULT NULL ,
+  `PASSWORD` VARCHAR(45) NULL DEFAULT NULL ,
+  PRIMARY KEY (`O_ID`) ,
+  UNIQUE INDEX `id_UNIQUE` (`O_ID` ASC) )
 ENGINE = InnoDB
+AUTO_INCREMENT = 8
 DEFAULT CHARACTER SET = latin1;
 
-SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `report`.`task_details`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `report`.`task_details` ;
+
+CREATE  TABLE IF NOT EXISTS `report`.`task_details` (
+  `O_ID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `U_ID` INT(11) NULL DEFAULT NULL ,
+  `PROJECT_ID` INT(11) NULL DEFAULT NULL ,
+  `WEEK_NO` INT(11) NULL DEFAULT NULL ,
+  `PLANNED_TASK` VARCHAR(45) NULL DEFAULT NULL ,
+  `COMPLETED_TASK` VARCHAR(45) NULL DEFAULT NULL ,
+  `HOLD_TASK` VARCHAR(45) NULL DEFAULT NULL ,
+  `INPROGRESS_TASK` VARCHAR(45) NULL DEFAULT NULL ,
+  `RISK_DESC` VARCHAR(700) NULL DEFAULT NULL ,
+  `RISK_RESP` VARCHAR(45) NULL DEFAULT NULL ,
+  `SHARED_RESOURCES` VARCHAR(745) NULL DEFAULT NULL ,
+  `BILLED_RESOURCES` VARCHAR(45) NULL DEFAULT NULL ,
+  `PROJECT_INC` VARCHAR(45) NULL DEFAULT NULL ,
+  `PROJECT_DEC` VARCHAR(45) NULL DEFAULT NULL ,
+  PRIMARY KEY (`O_ID`) ,
+  INDEX `uid_idx` (`U_ID` ASC) ,
+  INDEX `pid_idx` (`PROJECT_ID` ASC) ,
+  CONSTRAINT `pid`
+    FOREIGN KEY (`PROJECT_ID` )
+    REFERENCES `report`.`project_details` (`OID` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `uid`
+    FOREIGN KEY (`U_ID` )
+    REFERENCES `report`.`user_details` (`O_ID` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = latin1;
+
+USE `report` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
